783d4fabb27fdad93ac1a7745fa07b098668e822
pedropsoares <pedropaulos.1998@gmail.com>
2020-05-11 19:19:17 -0300

    bug fixed

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Abas from './pages/Abas';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home}/>
       <Route path="/login" component={Login}/>
-      <Route path="/abas" component={Abas}/>
     </Switch>
   </BrowserRouter>
 );
