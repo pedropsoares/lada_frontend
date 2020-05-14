20b08b4344f20edf8152b716a1822c98ef07b9e8
pedropsoares <pedropaulos.1998@gmail.com>
2020-05-14 12:53:31 -0300

    home page of recruiters and authentication implemented

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import PrivateRoutes from './auth';
+import { CompanyRoutes, RecruiterRoutes } from './auth';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register'
 import HomeCompany from './pages/Company/HomeCompany'
-
-
+import HomeRecruiter from './pages/Recruiter/HomeRecruiter'
 
 const Routes = () => (
   <BrowserRouter>
@@ -15,7 +14,8 @@ const Routes = () => (
       <Route path="/" exact component={Home}/>
       <Route path="/login" component={Login}/>
       <Route path="/register" component={Register}/>
-      <PrivateRoutes path="/company" component={HomeCompany}/>
+      <CompanyRoutes path="/company" component={HomeCompany}/>
+      <RecruiterRoutes path="/recruiter" component={HomeRecruiter}/>
     </Switch>
   </BrowserRouter>
 );
