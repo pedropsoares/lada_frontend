89923b9dba65e5a8ca0c5256a140ca005af7c139
pedropsoares <pedropaulos.1998@gmail.com>
2020-05-18 20:40:19 -0300

    ...

--- src/services/api.js
+++ src/services/api.js
@@ -5,12 +5,3 @@ const api = axios.create({
 });
 
 export default api;
-
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://localhost:3000/",
-});
-
-
-export default api;
